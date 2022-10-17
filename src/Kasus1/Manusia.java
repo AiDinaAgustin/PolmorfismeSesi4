699948743a1aea059ef47403d9b8104be0579eb0
AiDinaAgustin <95268109+AiDinaAgustin@users.noreply.github.com>
2022-10-17 21:11:04 +0700

    Create Manusia.java

---
+++ src/Kasus1/Manusia.java
@@ -0,0 +1,38 @@
+package Kasus1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Manusia {
+    protected String nama;
+    protected int umur;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+    
+    //Method ini akan di override oleh Mahasiswa
+    public void rename(String nama){
+        System.out.println(this.nama+" berganti nama menjadi "+nama);
+        this.nama = nama;
+    }
+}
